0d72a1a22fef83727a50cc00e38ba1fe551670f5
Osvaldas Bernatavičius <osvaldas.bernatavicius@gmail.com>
2021-01-04 21:21:30 +0200

    CreationTimestamp and UpdateTimestamp added.

--- src/main/java/com/notes/notelist/DTO/NoteDTO.java
+++ src/main/java/com/notes/notelist/DTO/NoteDTO.java
@@ -4,13 +4,17 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @ApiModel(description = "Details about the note.")
 public class NoteDTO {
-    @ApiModelProperty(notes = "The unique id of the note.")
     private Integer id;
-    @ApiModelProperty(notes = "The note's title.")
     private String title;
-    @ApiModelProperty(notes = "The note's status (done or not).")
     private boolean done;
+
+    @ApiModelProperty(hidden = true)
+    private LocalDateTime createdDate;
+    @ApiModelProperty(hidden = true)
+    private LocalDateTime lastUpdatedDate;
 }
