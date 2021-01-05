e2af38b53e5c705a198884580e266d38be175806
osber1 <osvaldas.bernatavicius@gmail.com>
2021-01-05 17:09:59 +0200

    Small fix.

--- src/main/java/com/notes/notelist/jwt/JwtConfig.java
+++ src/main/java/com/notes/notelist/jwt/JwtConfig.java
@@ -4,10 +4,12 @@ import com.google.common.net.HttpHeaders;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "application.jwt")
+@Component
 public class JwtConfig {
     private String secretKey;
     private String tokenPrefix;
