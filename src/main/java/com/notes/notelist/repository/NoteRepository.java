6c7d9b0649cd07144c15bd56e1addb4783953015
osber1 <osvaldas.bernatavicius@gmail.com>
2021-01-07 14:10:30 +0200

    Minor update

--- src/main/java/com/notes/notelist/repository/NoteRepository.java
+++ src/main/java/com/notes/notelist/repository/NoteRepository.java
@@ -3,6 +3,6 @@ package com.notes.notelist.repository;
 import com.notes.notelist.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NoteRepository extends JpaRepository <Note, Integer> {
+public interface NoteRepository extends JpaRepository<Note, Integer> {
 
 }
